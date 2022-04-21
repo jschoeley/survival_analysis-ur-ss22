768fb01ad8d134db1c874143132920f1a023238e
Jonas Schöley <jschoeley@gmail.com>
2022-04-21 08:38:46 +0200

    Add Week 2

---
+++ 02-likelihood_estimation/src/interactive_likelihood_demonstration.R
@@ -0,0 +1,219 @@
+# Interactive Graphical Demonstrations for Survival Analysis
+
+# Init ------------------------------------------------------------
+
+library(shiny)
+library(ggplot2)
+library(patchwork)
+library(flexsurv)
+
+
+# Data ------------------------------------------------------------
+
+bc <-
+  data.frame(
+    positive = c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+                 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+    weeks = c(19, 25, 30, 
+              34, 37, 46, 47, 51, 56, 57, 61, 66, 67, 74, 78, 86, 122, 123, 
+              130, 130, 133, 134, 136, 141, 143, 148, 151, 152, 153, 154, 156, 
+              162, 164, 165, 182, 189, 22, 23, 38, 42, 73, 77, 89, 115, 144),
+    censored = c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+                 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                 0, 0, 0, 0, 0, 0, 0, 0, 1)
+  )
+
+# Functions -------------------------------------------------------
+
+GompertzHazard <- function (x, a, b) {
+  a*exp(b*x)
+}
+GompertzSurvival <- function (x, a, b) {
+  if (b != 0) {
+    exp(-a/b*(exp(b*x)-1))  
+  } else {
+    exp(-a*x)
+  }
+}
+GompertzQuantile <- function (p, a, b) {
+  flexsurv::qgompertz(p, shape = b, rate = a)
+}
+GompertzDensity <- function (x, a, b) {
+  flexsurv::dgompertz(x, shape = b, rate = a)
+}
+GompertzLogDensity <- function (x, a, b) {
+  flexsurv::dgompertz(x, shape = b, rate = a, log = TRUE)
+}
+GompertzSample <- function (n, a, b) {
+  flexsurv::rgompertz(n, shape = b, rate = a)
+}
+
+# UI --------------------------------------------------------------
+
+# define user interface for plotting survival quantities
+ui <- fluidPage(
+  
+      # Application title
+    titlePanel("Likelihood maximization"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+          checkboxInput("showtrue", label = 'Show truth', value = TRUE),
+          conditionalPanel(
+            "input.showtrue == true",
+            sliderInput("true_rate",
+                        "Rate (x 100,000)",
+                        min = 0,
+                        max = 3000,
+                        value = 1500),
+            sliderInput("true_slope",
+                        "Relative derivatve",
+                        min = -0.05,
+                        max = 0.05,
+                        value = 0)
+          ),
+          actionButton('randomize', 'Randomize Parameters!', width = '100%'),
+          actionButton('sample', 'Draw Sample!', width = '100%'),
+          sliderInput("guess_rate",
+                      "Guess rate",
+                      min = 0,
+                      max = 3000,
+                      value = 1500),
+          sliderInput("guess_b",
+                      "Guess derivative",
+                      min = -0.05,
+                      max = 0.05,
+                      value = 0)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+          tabsetPanel(
+            id = "tabset",
+            tabPanel("Distribution", plotOutput("distPlot")),
+            tabPanel("Likelihood contributions", tableOutput('table')),
+          ),
+           h5('Log-likelihood'),
+           verbatimTextOutput('likelihood')
+        )
+    )
+)
+
+# Server ----------------------------------------------------------
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+
+  cnst <- list(
+    rate_scale = 1e5,
+    min_rate = 0,
+    max_rate = 3000,
+    min_b = -0.05,
+    max_b = 0.05,
+    n_sample = 20
+  )
+  
+  v <- reactiveValues()
+
+  observeEvent(input$randomize, {
+    updateSliderInput(session, 'true_rate',
+                      value = runif(1, cnst$min_rate, cnst$max_rate))
+    updateSliderInput(session, 'true_slope',
+                      value = runif(1, cnst$min_b, cnst$max_b))
+  })
+  
+  observeEvent(input$sample, {
+    v$samples <-
+      GompertzSample(cnst$n_sample,
+                     input$true_rate/cnst$rate_scale, input$true_slope)
+  })
+  
+  params <- reactive({
+    l <- list(
+      true_a = input$true_rate/cnst$rate_scale,
+      true_b = input$true_slope,
+      guess_a = input$guess_rate/cnst$rate_scale,
+      guess_b = input$guess_b
+    )
+    l$samples <- v$samples
+    if (is.null(v$samples)) {
+      l$samples <- as.numeric(NA)
+    }
+    l
+  })
+  
+  output$distPlot <- renderPlot({
+    
+    plot_fx <-
+      ggplot() +
+      list(
+        if (input$showtrue) {
+        geom_function(
+          fun = GompertzDensity,
+          args = list(a = params()$true_a, b = params()$true_b)
+        )} else { NULL },
+        geom_function(
+          fun = GompertzDensity,
+          args = list(a = params()$guess_a, b = params()$guess_b),
+          color = 'blue', lty = 2
+        ),
+        annotate('point', x = params()$samples, y = 0.001),
+        scale_x_continuous(limits = c(0, 100), expand = c(0,0)),
+        scale_y_continuous(limits = c(0, 0.03), expand = c(0,0)),
+        labs(x = 'Time x', y = 'Probability density f(x)'),
+        theme_classic(),
+        coord_cartesian(clip = 'off')
+      )
+    
+    plot_hx <-
+      ggplot() +
+      list(
+        if (input$showtrue) {
+          geom_function(
+            fun = GompertzHazard,
+            args = list(a = params()$true_a, b = params()$true_b)
+          )} else { NULL },
+        geom_function(
+          fun = GompertzHazard,
+          args = list(a = params()$guess_a, b = params()$guess_b),
+          color = 'blue', lty = 2
+        ),
+        scale_x_continuous(limits = c(0, 100),
+                           expand = c(0,0)),
+        scale_y_continuous(limits = c(0, 0.1), expand = c(0,0),
+                           labels = scales::label_number(scale = 1e5)),
+        labs(x = 'Time x', y = 'Hazard rate h(x) (x 100,000)'),
+        theme_classic(),
+        coord_cartesian(clip = 'off')
+      )
+    
+    plot_hx + plot_fx +
+    patchwork::plot_layout(nrow = 2)
+    
+  })
+  
+  output$likelihood <- renderPrint({
+    
+    loglikelihood <-
+      sum(GompertzLogDensity(params()$samples, params()$guess_a, params()$guess_b))
+    loglikelihood
+    
+  })
+  
+  output$table <- renderTable({
+    
+    loglikelihood <-
+      GompertzLogDensity(
+        params()$samples, params()$guess_a, params()$guess_b
+      )
+    data.frame(`Observation x` = params()$samples,
+               `Log-likelihood contribution` = loglikelihood)
+    
+  })
+  
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
---
+++ 02-likelihood_estimation/src/labwork.R
@@ -0,0 +1,103 @@
+# Init ------------------------------------------------------------
+
+library(tidyverse)
+library(survival)
+
+# Load data -------------------------------------------------------
+
+bc <- read_csv(file = '02-likelihood_estimation/dat/breastcancer.csv')
+
+# Explore data ----------------------------------------------------
+
+head(bc)
+glimpse(bc)
+summary(bc)
+
+# Prepare data ----------------------------------------------------
+
+bc_clean <-
+  bc %>%
+  mutate(
+    positive = factor(
+      positive, levels = c(0, 1),
+      labels = c('negative', 'positive')
+    ),
+    event = 1 - censored
+  )
+
+# Fit Gompertz distribution via ML --------------------------------
+
+GompertzSurvival <- function (x, a, b, log = FALSE) {
+  if (log) {
+    (a-a*exp(b*x))/b
+  } else {
+    exp((a-a*exp(b*x))/b)
+  }
+}
+
+GompertzHazard <- function (x, a, b, log = FALSE) {
+  if (log) {
+    log(a)+b*x
+  } else {
+    a*exp(b*x)
+  }
+}
+
+GompertzDensity <- function (x, a, b, log = FALSE) {
+  log_density <-
+    GompertzHazard(x, a, b, log = TRUE) +
+    GompertzSurvival(x, a, b, log = TRUE)
+  if (log) {
+    log_density
+  } else {
+    exp(log_density)
+  }
+}
+
+GompertzLikelihood <- function (theta, x, log = TRUE) {
+  loglike <-
+    sum(
+      GompertzDensity(x, exp(theta['a']), theta['b'],
+                      log = TRUE)
+    )
+  if (log) {
+    loglike
+  } else {
+    exp(loglike)
+  }
+}
+
+x <- bc_clean$weeks[bc_clean$positive=='positive'&!bc_clean$censored]
+
+theta_ml_fit <- optim(
+  par = c(a = log(1/mean(x)), b = -0.01),
+  GompertzLikelihood,
+  x = x, method = 'BFGS', control = list(fnscale = -1)
+)
+
+gompertz_survival <-
+  tibble(
+    x = 0:150,
+    Sx = GompertzSurvival(
+      x = x,
+      a = exp(theta_ml_fit$par['a']),
+      b = theta_ml_fit$par['b']
+    ),
+    fx = GompertzDensity(
+      x = x,
+      a = exp(theta_ml_fit$par['a']),
+      b = theta_ml_fit$par['b']
+    )
+  )
+
+ggplot(gompertz_survival) +
+  geom_line(aes(x = x, y = Sx)) +
+  annotate('point', x = x, y = 0)
+
+# find quantile values
+uniroot(
+  function (x, a, b) {GompertzSurvival(x, a, b)-0.75},
+  interval = c(0, 150),
+  a = exp(theta_ml_fit$par['a']),
+  b = theta_ml_fit$par['b']
+)
